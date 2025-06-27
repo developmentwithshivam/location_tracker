ebeeb6b8ee15722cc0d198d579d2bd4916097068
developmentwithshivam <developmentwithshivam@gmail.com>
2025-06-27 14:26:00 +0530

    9

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react"
 export default defineConfig(({mode})=>({
-  base:  mode === 'production' ? '/location/' : '/',
+  base:  mode === 'production' ? '/Location/' : '/',
   plugins: [react()],
 }))
